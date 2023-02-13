3430f24a7481f62c07e980a922ed3ac6b390307a
CxRedix <1135058679@qq.com>
2023-02-13 22:07:39 +0800

    Reverted back to guid from master branch, and moved it to another branch

--- Src/ModifierHandle.cs
+++ Src/ModifierHandle.cs
@@ -3,17 +3,16 @@
 /// <summary>
 ///     A safety handle for modifiers used as an identifier to know which modifier it is representing
 /// </summary>
+
 public readonly struct ModifierHandle
 {
-	ModifierHandle( int id ) =>
-		_id = id;
+	ModifierHandle( Guid guid ) =>
+		_guid = guid;
 
 	public override int GetHashCode() =>
-		_id.GetHashCode();
+		_guid.GetHashCode();
 
-	static readonly ConcurrentIntIncrementor _incrementor = new();
+	readonly Guid _guid;
 
-	readonly int _id;
-
-	public static ModifierHandle New() => new( _incrementor.Next() );
+	public static ModifierHandle New() => new( Guid.NewGuid() );
 }
