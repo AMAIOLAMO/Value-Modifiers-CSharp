017d35bbb565ac8f4d1cd9f99b225136b742c842
CxRedix <1135058679@qq.com>
2023-02-14 15:20:33 +0800

    [Test Pass] Switched Int incrementor to ulong

---
+++ Src/ConcurrentUlongIncrementor.cs
@@ -0,0 +1,18 @@
+﻿namespace CxUtils.ValueModifiers;
+
+internal class ConcurrentUlongIncrementor
+{
+	public ulong Next()
+	{
+		lock ( _incrementalLock )
+		{
+			++_internalIncrementor;
+
+			return _internalIncrementor;
+		}
+	}
+
+	readonly object _incrementalLock = new();
+
+	ulong _internalIncrementor;
+}
--- Src/ModifierHandle.cs
+++ Src/ModifierHandle.cs
@@ -3,17 +3,23 @@
 /// <summary>
 ///     A safety handle for modifiers used as an identifier to know which modifier it is representing
 /// </summary>
-public readonly struct ModifierHandle
+public readonly struct ModifierHandle : IEquatable<ModifierHandle>
 {
-	ModifierHandle( int id ) =>
+	ModifierHandle( ulong id ) =>
 		_id = id;
+
+	public bool Equals( ModifierHandle other ) =>
+		_id == other._id;
 
 	public override int GetHashCode() =>
 		_id.GetHashCode();
 
-	static readonly ConcurrentIntIncrementor _incrementor = new();
+	public override bool Equals( object? obj ) =>
+		obj is ModifierHandle other && Equals( other );
 
-	readonly int _id;
+	static readonly ConcurrentUlongIncrementor _incrementor = new();
+
+	readonly ulong _id;
 
 	public static ModifierHandle New() => new( _incrementor.Next() );
 }
