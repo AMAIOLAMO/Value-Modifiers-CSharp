92a789162bfa8c2c1b04e35ee0a3a353c69c58a1
CxRedix <1135058679@qq.com>
2023-02-17 15:26:07 +0800

    Implemented basic priority applier

--- Src/Applier/Concrete/PriorityApplier.cs
+++ Src/Applier/Concrete/PriorityApplier.cs
@@ -1,12 +1,69 @@
 ﻿namespace CxUtils.ValueModifiers;
 
+// TODO: require unit tests for this applier
+// TODO: require documentation
 public class PriorityApplier<T> : IApplier<T>
 {
-	public T ApplyTo( T value ) =>
-		throw new NotImplementedException();
+	public T ApplyTo( T value )
+	{
+		T resultValue = value;
 
-	public void Clear() =>
-		throw new NotImplementedException();
+		EnsureSorted();
+
+		foreach ( PriorityModifier<T> priorityModifier in _priorityModifiers )
+			resultValue = priorityModifier.ApplyTo( resultValue );
+
+		return resultValue;
+	}
+
+	public void Clear()
+	{
+		_priorityModifiers.Clear();
+		_sorted = true;
+	}
 
 	public int Count { get; }
+
+	/// <summary>
+	///     Sorts the modifiers
+	/// </summary>
+	public void Sort() =>
+		EnsureSorted();
+
+	public void Add( PriorityModifier<T> modifier )
+	{
+		_priorityModifiers.Add( modifier );
+		OnItemsUpdated();
+	}
+
+	public void RemoveAt( int index )
+	{
+		_priorityModifiers.RemoveAt( index );
+		OnItemsUpdated();
+	}
+
+	readonly List<PriorityModifier<T>> _priorityModifiers = new();
+
+	bool _sorted;
+
+	void OnItemsUpdated()
+	{
+		switch ( _priorityModifiers.Count )
+		{
+			case 0:
+			case 1:
+				_sorted = true;
+
+				break;
+		}
+	}
+
+	void EnsureSorted()
+	{
+		if ( _sorted )
+			return;
+
+		_priorityModifiers.Sort();
+		_sorted = true;
+	}
 }
